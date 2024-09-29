7b07154c4093fe628ada4eaa023e21ddd38579ee
MarcTy <marcadriel@aol.com>
2024-09-28 21:21:20 -0600

    sensor-pub

---
+++ sensor-pub-py/tests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# For running more tests just add below:
+
+# echo "Running some test"
+# npm run test || { echo "Tests failed, exiting"; exit 1; }
+
+
+# Display completion.
+echo "Tests completed successfully."
